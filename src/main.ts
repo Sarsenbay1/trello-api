bd05d225e042ae0c31a08428f156a5b37aa19f5b
Sarsenbay1 <88890025m@gmail.com>
2024-10-29 17:20:29 +0600

    swagger

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,23 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { EnvironmentVariables } from './environment-variables';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService<EnvironmentVariables, true>);
 
-  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Trello')
+    .setDescription('The trello API')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(configService.get<number>('PORT') || 3000);
 
   console.log(`Server ok PORT ${configService.get<number>('PORT') || 3000}`);
